0bea1a9bd537cefe4d8ac5f4aae00c4147be5980
Soniksub <sparlant69@gmail.com>
2025-01-12 23:22:51 +0100

    commit initial

---
+++ spring/web/sa-backend/src/main/java/tech/soniksub/sa/controller/TestController.java
@@ -0,0 +1,20 @@
+package tech.soniksub.sa.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping( path = "test" )
+
+
+public class TestController {
+
+    @GetMapping( path = "chaine" )
+    public String getString() {
+        return "hello from SA";
+    }
+}
+
+
+
